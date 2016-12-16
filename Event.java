27478096355cef54320b1e68d0e4b5e703650184
MShaban95 <s-mahmoud.almaz@zewailcity.edu.eg>
2016-12-16 18:24:16 +0200

    Update Event.java

--- Event.java
+++ Event.java
@@ -1,15 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package pkgtry;
 
-
-/**
- *
- * @author MShaban
- */
 import java.util.ArrayList;
 public class Event {
     
