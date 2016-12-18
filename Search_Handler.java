1c33a212b0109ebbe0aa39d8710eb2ae2e9902ec
ElGondy <elgendy01@gmail.com>
2016-12-18 09:56:44 +0200

    Update Search_Handler.java

--- Search_Handler.java
+++ Search_Handler.java
@@ -2,5 +2,32 @@ import java.sql.*;
 
 public class Search_Handler {
 
+	public void Search_Event_By_Name(String Name){
+		
+		
+	}
 	
+	
+	
+	public void Search_User(String Name){
+		
+		
+	}
+	
+	
+	
+	public void Search_Event_By_Category(String Category){
+		
+		
+	}
+	
+	
+	
+	public void Search_Event_By_Date(String Name){
+		
+		
+	}
+	
+
+
 }
